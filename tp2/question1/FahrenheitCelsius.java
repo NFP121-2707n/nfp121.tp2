f20d6b7f1837c32f82a9b806b6f54db370795004
Youssef Antoury <youssef.antoury@isae.edu.lb>
2023-12-13 23:28:59 +0200

    question 1

--- tp2/question1/FahrenheitCelsius.java
+++ tp2/question1/FahrenheitCelsius.java
@@ -1,5 +1,4 @@
 package question1;
-
 /**
  * Décrivez votre classe FahrenheitCelsius ici.
  * 
@@ -8,34 +7,39 @@ package question1;
  */
 public class FahrenheitCelsius {
 
-	/**
-	 * le point d'entrée de cette application, dont le commentaire est à
-	 * compléter
-	 * 
-	 * @param args
-	 *            ...
-	 */
-	public static void main(String[] args) {
-		// pour tous les paramètres de la ligne de commande
-		int fahrenheit = 0;
-		float celsius = 0;
-		System.out.println(fahrenheit + "\u00B0F -> " + celsius + "\u00B0C"); // ligne,
-																				// format
-																				// imposés
-																				
-	}
+    /**
+     * le point d'entrée de cette application, dont le commentaire est à
+     * compléter
+     * 
+     * @param args
+     *            ...
+     */
+    public static void main(String[] args) {
+        // pour tous les paramètres de la ligne de commande
+        for(int i=0; i<args.length;i++){
+            
+        int fahrenheit = Integer.valueOf(args[i]);
+        float celsius = fahrenheitEnCelsius(Integer.valueOf(args[i]));
+        System.out.println(fahrenheit + "\u00B0F -> " + celsius + "\u00B0C"+i); // ligne,
+        }                                                                      // format
+                                                                                // imposés
+                                                                                
+    }
 
-	/**
-	 * la méthode à compléter.
-	 * 
-	 * @param f
-	 *            la valeur en degré Fahrenheit
-	 * @return la conversion en degré Celsius
-	 */
-	public static float fahrenheitEnCelsius(int f) {
-
-		return 0.F; // à compléter en remplaçant ce return 0.F par la fonction
-					// de conversion
-	}
+    /**
+     * la méthode à compléter.
+     * 
+     * @param f
+     *            la valeur en degré Fahrenheit
+     * @return la conversion en degré Celsius
+     */
+    public static float fahrenheitEnCelsius(int f) {
+        float r1=(float)5;
+        float r2=(float)9;
+        float d1=(float)32;
+        float r=(r1/r2)*(f-d1);
+        return  (float)(Math.floor(r*10.0)/10.0); // à compléter en remplaçant ce return 0.F par la fonction
+                    // de conversion
+    }
 
 }
